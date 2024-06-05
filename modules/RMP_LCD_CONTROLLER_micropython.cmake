69058771953f3309bed1538c34dd011a84fb0823
John Gentilin <gentijo@gmail.com>
2024-06-05 18:29:17 +0000

    Added deleted files from directory move

--- modules/RMP_LCD_CONTROLLER_micropython.cmake
+++ modules/RMP_LCD_CONTROLLER_micropython.cmake
@@ -4,12 +4,12 @@ get_filename_component(MY_MODULE_DIR ../../../../modules ABSOLUTE)
 get_filename_component(MOD_ROS_MICROPY_DIR ${MY_MODULE_DIR}/libROSMicroPy ABSOLUTE)
 include (${MOD_ROS_MICROPY_DIR}/micropython.cmake)
 
-get_filename_component(MOD_ROS_MICROPY_GUI_DIR ${MY_MODULE_DIR}/libROSMicroPyGUI ABSOLUTE)
+get_filename_component(MOD_ROS_MICROPY_GUI_DIR ${MY_MODULE_DIR}/libMicroPyGUI ABSOLUTE)
 include (${MOD_ROS_MICROPY_GUI_DIR}/micropython.cmake)
 
 get_filename_component(MOD_MICROPY_HELPERS_DIR ${MY_MODULE_DIR}/micropython-helpers ABSOLUTE)
 include (${MOD_MICROPY_HELPERS_DIR}/micropython.cmake)
 
-get_filename_component(MOD_MICRO_EMACS_DIR ${MY_MODULE_DIR}/lib_MicroPyEmacs ABSOLUTE)
+#get_filename_component(MOD_MICRO_EMACS_DIR ${MY_MODULE_DIR}/lib_MicroPyEmacs ABSOLUTE)
 #include (${MOD_MICRO_EMACS_DIR}/micropython.cmake)
 
--- modules/libROSMicroPyGUI/layout/JSONForms_LVGL_Renderer.h
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-
-#include "py/runtime.h"
-#include "py/obj.h"
-
-mp_obj_t parseLayoutDefinition(mp_obj_t obj_in, bool debug);
